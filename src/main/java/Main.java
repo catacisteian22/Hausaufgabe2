5391b49cbe813211441434cb9979377b8f7f69ff
Cătălina Cisteian <84501255+catacisteian22@users.noreply.github.com>
2021-10-18 13:47:17 +0300

    Final commit with Junit tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,23 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) {
-        Benutzer user = new Benutzer();
-        user.setFirstName("Catalina");
-        user.setLastName("Cisteian");
+        Benutzer benutzer = new Benutzer();
+        benutzer.setFirstName("Catalina");
+        benutzer.setLastName("Cisteian");
 
         Fussball fussball = new Fussball();
         Basketball basketball = new Basketball();
 
-        user.sport.add(fussball);
-        user.sport.add(basketball);
+        benutzer.sport.add(fussball);
+        benutzer.sport.add(basketball);
 
-        double totalTime = user.kalkuliereZeit();
-        double footballTime = user.kalkuliereZeit(fussball);
-        double averageTime = user.Durchschnitt();
+        double totalTime = benutzer.kalkuliereZeit();
+        double sportTime = benutzer.kalkuliereZeit(benutzer.sport.get(benutzer.sport.indexOf(basketball)));
+        double averageTime = benutzer.Durchschnitt();
 
-        System.out.println("User firstname is: " + user.getFirstName() + "; User lastname is: " + user.getLastName());
+        System.out.println("User firstname is: " + benutzer.getFirstName() + "; User lastname is: " + benutzer.getLastName());
         System.out.println("User total time sport: " + totalTime);
-        System.out.println("User one sport time: " + footballTime);
+        System.out.println("User one sport time: " + sportTime);
         System.out.println("User average time sport: " + averageTime);
     }
 }
--- src/test/java/BenutzerTest.java
+++ src/test/java/BenutzerTest.java
@@ -25,4 +25,43 @@ public class BenutzerTest {
 
         assertEquals(testTotalTime, totalTime, 0);
     }
+
+    @Test
+    public void testTimeOneSport() {
+        Benutzer benutzer = new Benutzer();
+
+        Fussball fussball = new Fussball();
+        Basketball basketball = new Basketball();
+        Hindernislauf hinder = new Hindernislauf();
+        Hochsprung hoch = new Hochsprung();
+
+        benutzer.sport.add(fussball);
+        benutzer.sport.add(hinder);
+        benutzer.sport.add(hoch);
+
+        double sportTime = benutzer.kalkuliereZeit(benutzer.sport.get(benutzer.sport.indexOf(hoch)));
+        double sportTimeTest = hoch.kalkuliereZeit();
+
+        assertEquals(sportTimeTest, sportTime, 0);
+    }
+
+    @Test
+    public void testAverageTimeSport() {
+        Benutzer benutzer = new Benutzer();
+
+        Fussball fussball = new Fussball();
+        Basketball basketball = new Basketball();
+        Hindernislauf hinder = new Hindernislauf();
+        Hochsprung hoch = new Hochsprung();
+
+        benutzer.sport.add(fussball);
+        benutzer.sport.add(hinder);
+        benutzer.sport.add(hoch);
+        benutzer.sport.add(basketball);
+
+        double averageTime = benutzer.Durchschnitt();
+        double averageTimeTest = (fussball.kalkuliereZeit() + hinder.kalkuliereZeit() + hoch.kalkuliereZeit() + basketball.kalkuliereZeit()) / benutzer.sport.size();
+
+        assertEquals(averageTimeTest, averageTime, 0);
+    }
 }
